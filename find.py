23b46945b853fb1866201707a462afa286046520
Sofia Ergenova <113054061+SofiaErgenova@users.noreply.github.com>
2022-12-22 22:55:09 +0300

    добавление return -1
    изменения в модуле Поиска: добавление else:  return -1

--- find.py
+++ find.py
@@ -1,15 +1,23 @@
+
 def find_student(id_student: int)-> str:
     """поиcк по id ученика возвращет строку [айди имя фамилия класс литера]"""
     
-    with open('student.txt', "r", encoding='UTF-8') as data_file:
+    with open('students.txt', "r", encoding='UTF-8') as data_file:
         for line in data_file.readlines():
             if str(id_student) in line:
-                return line
+                i = 0
+                id_data = ''
+                while line[i] != "|":
+                    id_data = id_data + line[i]
+                    i += 1
+                if id_student == int(id_data):
+                    return line
+        return -1
 
 
 def find_id_student(surname: str)-> int:
     """поиcк по фамилии возвращет id ученика"""
-    with open('student.txt', "r", encoding='UTF-8') as data_file:
+    with open('students.txt', "r", encoding='UTF-8') as data_file:
         for line in data_file.readlines():
             if surname in line:
                 i = 0
@@ -17,5 +25,7 @@ def find_id_student(surname: str)-> int:
                 while line[i] != "|":
                     id_student = id_student + line[i]
                     i += 1
-    return int(id_student) 
+                return int(id_student)
+        else:
+            return -1
 
