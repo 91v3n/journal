7c43b75e6edde17c4fdd91974ed62d085fd4bd58
Eugenia4321 <113255505+Eugenia4321@users.noreply.github.com>
2022-12-23 17:48:22 +0500

    Update check.py

--- check.py
+++ check.py
@@ -1,4 +1,8 @@
-def check_name(student_name:str='петя')->bool:   
+def check_name(student_name:str='петя')->bool:
+    """возвращает true если строка из букв от 4 до 15 символов
+    """   
+    if not student_name.isalpha():
+        return False
     if len(student_name)>15 or len(student_name)<4:
         return False
     else:
@@ -6,6 +10,10 @@ def check_name(student_name:str='петя')->bool:
 
 
 def cheсk_second_name(student_second_name:str='петров')->bool:
+    """возвращает true если строка из букв от 3 до 15 символов
+    """   
+    if not student_second_name.isalpha():
+        return False
     if len(student_second_name)>15 or len(student_second_name)<2:
         return False
     else:
@@ -13,6 +21,8 @@ def cheсk_second_name(student_second_name:str='петров')->bool:
 
 
 def check_class(class_number:str)->bool:
+    """возвращает true если строка из цирфы от 1 до 11 
+    """   
     if class_number.isdigit() == False:
         return False	
     if int(class_number)>11 or int(class_number)<1:
@@ -22,6 +32,8 @@ def check_class(class_number:str)->bool:
 
 
 def check_class_name(name_class:str='а')->bool:
+    """возвращает true если строка из буквы в диапозоне алфавита от а до з включительно
+    """   
     class_name_list=['а','б','в','г','д','е','ж','з']
     if class_name_list.count(name_class)==0:
         return False
@@ -30,12 +42,18 @@ def check_class_name(name_class:str='а')->bool:
 
 
 def check_subject(subject : str='биология')->bool:	
+    """возвращает true если строка из букв от 3 до 15 символов
+    """   
+    if not subject.isalpha():
+        return False
     if len(subject)>15 or len(subject)<3:
         return False
     else:
         return True
 
 def student_id(id:str)->bool:
+    """возвращает true если строка из цифр от 4 до 6 символов
+    """  
     if id.isdigit() == False:
         return False
     if len(str(id))>6 or len(str(id))<4:
@@ -43,22 +61,32 @@ def student_id(id:str)->bool:
     else:
         return True
 
-def check_quarter(quarter:int=4)->bool:	
+def check_quarter(quarter:str)->bool:	
+    """возвращает true если строка из цифры от 1 до 4 включительно
+    """  
+    if quarter.isdigit() == False:
+        return False
     if int(quarter)>4 or int(quarter)<1:
         return False
     else:
         return True
 
-def check_grade(grade:int=5)->bool:	
+def check_grade(grade:str)->bool:	
+    """возвращает true если строка из цифры от 2 до 5 включительно
+    """  
+    if grade.isdigit() == False:
+        return False
     if int(grade)>5 or int(grade)<2:
         return False
     else:
         return True
 
-################
+
 
 def check_item_menu(item_menu:str)->bool:
-    if 	item_menu.isdigit() and int(item_menu)>0 and int(item_menu)<6:
+    """возвращает true если строка из цифры от 1 до 5 включительно
+    """  
+    if 	item_menu.isdigit() and int(item_menu)>0 and int(item_menu)<5:
         return True
     else:
         return False    
